ba3d22e3f44b141f9dd0df854e61aee0434fa6da
zmcneill <zmcneill@uncc.edu>
2021-05-07 10:55:25 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require('express')
 const User = require('./../models/users')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const passport = require('passport')
 
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
     let user = new User({
       name: req.body.name,
@@ -17,6 +18,14 @@ router.post('/', async (req, res) => {
       console.log(e)
       res.redirect('/register')
     }
+})
+
+router.post('/login', (req, res) => {
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next)
 })
 
 module.exports = router
