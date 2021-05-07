9e5592302874304a57c681b8488a633d2155f4b5
zmcneill <zmcneill@uncc.edu>
2021-05-07 10:55:32 -0400

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -1,6 +1,52 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const User = require('./models/users')
 
+module.exports = function(passport) {
+    passport.use(
+        new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+            User.findOne({ email: email })
+                .then(user => {
+                    if(!user) {
+                        return done(null, false, { message: 'No user with that email' })
+                    }
+
+                    bcrypt.compare(password, user.password, (err, isMatch) => {
+                        if(err) throw err
+
+                        if(isMatch) {
+                            return done(null, user)
+                        } else {
+                            return done(null, false, {message: 'Password incorrect'})
+                        }
+                    })
+                })
+                .catch(err => console.log(err))
+        })
+    )
+
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user)
+        })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
@@ -26,5 +72,4 @@ function initialize(passport, getUserByEmail, getUserById) {
         done(null, getUserById(id))
     })
 }
-
-module.exports = initialize
+*/
