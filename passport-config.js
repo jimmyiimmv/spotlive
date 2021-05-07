7d97d403931a5677f9ad076276896b220c61d5a0
zmcneill <zmcneill@uncc.edu>
2021-05-07 09:52:06 -0400

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -9,7 +9,7 @@ function initialize(passport, getUserByEmail, getUserById) {
         }
 
         try {
-            if (password == user.password) {
+            if (await bcrypt.compare(password, user.password)) {
                 return done(null, user)
             } else {
                 return done(null, false, {message: 'Password incorrect'})
