3565eeaa9bead70f1bb40c84b24f6889639030d5
jimmyiimmv <jimmytryharder@gmail.com>
2021-06-15 18:56:37 -0400

    cleaned up deprecated features

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Player = require('./models/player')
 const playerRouter = require('./routes/players')
-const userRouter = require('./routes/users')
 const methodOverride = require('method-override')
 const app = express()
-const passport = require('passport')
+
 const flash = require('express-flash')
 const session = require('express-session')
-const User = require('./models/users')
-require('./passport-config')(passport)
+
+
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
@@ -31,58 +30,15 @@ app.use(session({
   resave: false, 
   saveUninitialized: false
 }))
-app.use(passport.initialize())
-app.use(passport.session())
+
 app.use(methodOverride('_method'))
 
-
-app.get('/', checkNotAuthenticated, async (req, res) => {
+app.get('/', async (req, res) => {
   const players = await Player.find().sort({ createdAt: 'desc' })
   res.render('players/index', { players: players})
 })
 
-app.get('/home', checkAuthenticated, async (req, res) => {
-  const players = await Player.find().sort({ createdAt: 'desc' })
-  res.render('players/home', {players: players, name: req.user.name})
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('players/login.ejs')
-})
-
-app.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/home',
-    failureRedirect: '/login',
-    failureFlash: true
-  })(req, res, next)
-})
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('players/register.ejs')
-})
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
 app.use('/players', playerRouter)
-app.use('/users', userRouter)
 
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/home')
-  }
-  next()
-}
 
 app.listen(process.env.PORT || 5000)
